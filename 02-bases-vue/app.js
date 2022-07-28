02565bce4bf4343f8355cf24fe2ef441aae51b88
cristaldoplf <cristaldoplf@gmail.com>
2022-07-28 14:07:45 -0300

    metodos

--- 02-bases-vue/app.js
+++ 02-bases-vue/app.js
@@ -3,12 +3,23 @@
 // <p>{{ !true ? 'Activo' : 'Inactivo' }}</p>
 // `
 const app = Vue.createApp({
-  data(){
+  data() {
     return {
       message: 'Hola Mundo',
       quote: "I'm Batman",
       author: 'Bruce Wayne'
     }
+  },
+  methods: {
+    changeQuote(event) { //le ponemos el evento
+      console.log('Hola Mundo', event)
+      this.author = 'Pablo Cristaldo'
+      this.capitalize()
+    },
+    capitalize() {
+      this.quote = this.quote.toUpperCase()
+    }
   }
+
 })
 app.mount('#myApp')

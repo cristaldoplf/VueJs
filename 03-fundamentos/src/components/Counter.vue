385fccf64f371ec1dc377fd66b0f7218ecc2c188
cristaldoplf <cristaldoplf@gmail.com>
2022-07-28 21:31:59 -0300

    funciones de incrementar y decrementar un render de un entero en pantalla

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,6 +1,11 @@
 <template>
   <h2>Counter!!!</h2>
-  <p>{{ counter }}<sup>2</sup>={{ counter * counter }}</p>
+  <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
+
+  <div>
+    <button v-on:click="increase">+1</button>
+    <button v-on:click="decrease">-1</button>
+  </div>
 </template>
 
 <script>
@@ -11,8 +16,41 @@ export default {
       counter: 5,
     };
   },
+  methods: {
+    getSquareValue() {
+      return this.counter * this.counter;
+    },
+    increase(){
+      this.counter++
+    },
+    decrease(){
+      this.counter--
+    }
+  },
+  computed: {
+    squareCounter() {
+      console.log("computed squareCounter");
+      return this.counter * this.counter;
+    },
+  },
 };
 </script>
 
 <style>
+
+  button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+  }
+
+  button:hover{
+    background-color: rgba(127, 255, 212, 0.593);
+    transition: 0.3s ease-in-out;
+  }
+
 </style>

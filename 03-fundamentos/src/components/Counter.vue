13389a3b8d7739409c9d1ab3a6fadbd9698772ce
cristaldoplf <cristaldoplf@gmail.com>
2022-07-29 19:47:53 -0300

    aplicacion consumiendo una api

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,56 +1,67 @@
 <template>
-  <h2>Counter!!!</h2>
+  <h2>{{ customTitle }} {{ start }}</h2>
   <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
 
   <div>
-    <button v-on:click="increase">+1</button>
-    <button v-on:click="decrease">-1</button>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 1000,
+      validator(value) {
+        return value > 100;
+      },
+    },
+  },
   // name: "patito",
   data() {
     return {
-      counter: 5,
+      counter: this.start,
     };
   },
   methods: {
     getSquareValue() {
       return this.counter * this.counter;
     },
-    increase(){
-      this.counter++
+    increase() {
+      this.counter++;
     },
-    decrease(){
-      this.counter--
-    }
+    decrease() {
+      this.counter--;
+    },
   },
   computed: {
     squareCounter() {
       console.log("computed squareCounter");
       return this.counter * this.counter;
     },
+    customTitle() {
+      return this.title || "Counter";
+    },
   },
 };
 </script>
 
 <style>
+button {
+  background-color: #64bb87;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
 
-  button{
-    background-color: #64BB87;
-    border-radius: 5px;
-    color: white;
-    cursor: pointer;
-    margin: 0 5px;
-    padding: 5px 15px;
-    transition: 0.3s ease-in-out;
-  }
-
-  button:hover{
-    background-color: rgba(127, 255, 212, 0.593);
-    transition: 0.3s ease-in-out;
-  }
-
+button:hover {
+  background-color: rgba(127, 255, 212, 0.593);
+  transition: 0.3s ease-in-out;
+}
 </style>
